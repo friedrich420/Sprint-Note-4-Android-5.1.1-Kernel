b47f6d2948cb5fbceb06b0067e1e482c2930a397
friedrich420 <kelpidorou@yahoo.com>
2015-08-30 10:41:36 +0300

    Revert Sweep2Sleep ans Sweep2Wake

--- drivers/video/msm/mdss/mdss_samsung_dsi_panel_common.c
+++ drivers/video/msm/mdss/mdss_samsung_dsi_panel_common.c
@@ -176,10 +176,6 @@ static struct dsi_cmd panel_osc_type_read_cmds;
 extern int te_set_done;
 #endif
 
-#ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
-#include <linux/input/sweep2wake.h>
-#endif
-
 static struct mipi_samsung_driver_data msd;
 /*List of supported Panels with HW revision detail
  * (one structure per project)
@@ -568,8 +564,7 @@ void mdss_dsi_samsung_panel_reset(struct mdss_panel_data *pdata, int enable)
 		wmb();
 
 	} else {
-		if (!s2w_switch)
-			gpio_set_value((ctrl_pdata->rst_gpio), 0);
+		gpio_set_value((ctrl_pdata->rst_gpio), 0);
 
 	}
 }
